fc62955b77b7eca2b46451cdfbfc6cecbbf456bf
Ferguson Iyara <ferguson.iyara@redacreltd.com>
2023-02-23 11:00:06 +0100

    chore: make a change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
 import { config } from "dotenv";
 
 import routes from "./routes";
@@ -10,7 +9,6 @@ config();
 const PORT = process.env.PORT || 4000;
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
